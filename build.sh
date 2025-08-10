fdffa1fb83f1be10bce49f9fcd86c7bc633bde5f
= <=>
2025-08-10 17:46:36 +0900

    fix: #4: DBのデータ永続化ができていなかったので修正

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
+echo "===== build.sh START ====="
 # exit on error
-# set -o errexit
+set -o errexit
 
 # pip install -r requirements.txt
 # python manage.py collectstatic --no-input
@@ -11,3 +13,5 @@ python manage.py migrate
 python manage.py character_init
 # dockerの方でrunserverするので使わない
 # python manage.py runserver
+
+echo "===== build.sh END ====="
